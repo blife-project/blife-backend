34301f6d10d2714f1885da439b7ebcc965c4be57
ikhsan <ikhsanapriliano4@gmail.com>
2024-12-14 15:59:42 +0700

    chore: add disable absolute path on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint/eslint-plugin"],
+  plugins: ["@typescript-eslint/eslint-plugin", "import"],
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript"
   ],
   root: true,
   env: {
@@ -21,5 +23,6 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "import/no-absolute-path": "error"
   },
 };
